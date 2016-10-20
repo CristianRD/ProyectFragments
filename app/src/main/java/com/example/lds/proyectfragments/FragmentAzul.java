1852fb71f23a871429132370d8a6d84b1415d53c
Laboratorio de Software <lds@unac.edu.co>
2016-10-19 19:34:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lds/proyectfragments/FragmentAzul.java
@@ -0,0 +1,23 @@
+package com.example.lds.proyectfragments;
+
+/**
+ * Created by Cristian RD on 16/10/2016.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lds.proyectfragments.R;
+
+public class FragmentAzul extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_azul,container,false);
+    }
+}
